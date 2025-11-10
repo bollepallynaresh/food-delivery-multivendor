3782ba75cbe59333923e92d018516859a2fe67ec
bollepallynaresh <bollepally.naresh2019@gmail.com>
2025-11-10 17:21:05 +0530

    Update Firebase configuration with hardcoded values
    Replaced Firebase configuration with hardcoded values.

--- firebase.ts
+++ firebase.ts
@@ -27,14 +27,14 @@ export const initialize = (config: IFirebaseConfig): Messaging | null => {
     }
 
     const firebaseConfig = {
-      apiKey: config.FIREBASE_KEY,
-      authDomain: config.FIREBASE_AUTH_DOMAIN,
-      projectId: config.FIREBASE_PROJECT_ID,
-      storageBucket: config.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: config.FIREBASE_MSG_SENDER_ID,
-      appId: config.FIREBASE_APP_ID,
-      measurementId: config.FIREBASE_MEASUREMENT_ID,
-    };
+  apiKey: "AIzaSyBMC16d-mWpG9mtyX5ml67h0GZo2fhkwX4",
+  authDomain: "mana-kuntloor-1.firebaseapp.com",
+  databaseURL: "https://mana-kuntloor-1-default-rtdb.firebaseio.com",
+  projectId: "mana-kuntloor-1",
+  storageBucket: "mana-kuntloor-1.firebasestorage.app",
+  messagingSenderId: "338731555945",
+  appId: "1:338731555945:web:d9180f973623593d7496f8"
+};
 
     // Initialize Firebase
     const app: FirebaseApp = initializeApp(firebaseConfig);
